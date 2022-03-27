e32878e92f0680385ae43b58cdb5caefc32a5049
Wes T4N <wesleyt1998@gmail.com>
2022-03-27 18:48:00 -0400

    Updates

---
+++ component---src-pages-404-js-585fdbb8f186cc04ef3a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),l=n("Bl7J"),r=n("vrFN");t.default=function(){return o.a.createElement(l.a,null,o.a.createElement(r.a,{title:"404: Not found"}),o.a.createElement("h1",null,"404: The page you looked for doesn't exist."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-585fdbb8f186cc04ef3a.js.map
